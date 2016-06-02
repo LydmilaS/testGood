ff34d7fee3dea35334d89d08f843e74fdb197e44
LydmilaS <lydmilazoloto@gmail.com>
2016-06-02 12:37:42 +0300

    responsive

--- js/topmenu.js
+++ js/topmenu.js
@@ -1,3 +1,21 @@
 function responsiveMenu() {
     document.getElementsByClassName("topnav")[0].classList.toggle("responsive");
 }
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(e) {
+  if (!e.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    for (var d = 0; d < dropdowns.length; d++) {
+      var openDropdown = dropdowns[d];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
