f74f101074c547a2278840c05f0a5fb0f48640fb
LydmilaS <lydmilazoloto@gmail.com>
2016-06-08 11:42:58 +0300

    get friend object

--- vk/login.js
+++ vk/login.js
@@ -1,45 +1,61 @@
+
 // wp-test.local - domain my app
 
 function login() {
-	VK.Auth.login( authInfo, +2);
-	function authInfo(response) {
-		  if (response.session) {
+    VK.Auth.login( authInfo, +2);
+    function authInfo(response) {
+        if (response.session) {
+            $("#logout").show();
+            $("#get_friends").show();
+            $("#login").hide();
 
-		  	$("#logout").show();
-		  	$("#get_friends").show();
-		  	$("#login").hide();
-
-		    alert('user: '+response.session.mid);
+            alert('user: '+response.session.mid);
 		   
-		  } else {
+        } else {
             alert('not auth');
-		}
-	}
+        }
+    }
 }
 
-// VK.Observer.subscribe('auth.login', function f()
-// {
-// alert ();
-// });
-
 function logout() {
-	VK.Auth.logout();
-	location.reload();
-	alert('you left app');
+    VK.Auth.logout();
+    location.reload();
+    alert('you left app');
 }
 
 function getFiends() {
-    VK.Api.call('friends.get', {fields: 'status, sex'}, function(data) {
+    VK.Api.call('friends.get', {fields: 'sex'}, function(data) {
+        
         if (data.error) {
             alert(data.error.error_msg);
         } else {
             if (data.response.length > 0) {
-				for (i = 1; i < data.response.length; i++) {
-    					$('#list').append("<li>" + " " + data.response[i].last_name + " " + data.response[i].first_name + "</li>");
-		            }
-	            }
-	        }
-   	});
 
-   	localStorage.setItem();
+            for (i = 0; i < data.response.length; i++) {
+
+                var item = data.response[i].last_name + " " + data.response[i].first_name;
+                //создадим объект
+                var friend = {
+                    first_name: data.response[i].first_name,
+                    last_name: data.response[i].last_name
+                };
+				 
+                var serialFriend = JSON.stringify(friend); //сериализуем его
+				 
+                localStorage.setItem(i, serialFriend); //запишем его в хранилище по ключу "myKey"
+				 
+                var returnFriend = JSON.parse(localStorage.getItem(i)) //спарсим его обратно объект
+					
+                if(returnFriend) {
+                    $('#local').append("<li>" + " " + item + "</li>");
+                } else {
+                    $('#list').append("<li>" + " " + item + "</li>");
+                }
+	        }
+
+            } else {
+                alert('You don"t have friends:(')
+            }
+        }
+    });
 }
